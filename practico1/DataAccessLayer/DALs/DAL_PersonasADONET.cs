ae86a6fa7c391d983276990b94c813b61bb9586b
MarioJCaro <mario.caro@estudiantes.utec.edu.uy>
2023-09-06 11:54:56 -0300

    Entrega Practico 2 Mario Caro

---
+++ practico1/DataAccessLayer/DALs/DAL_PersonasADONET.cs
@@ -0,0 +1,119 @@
+﻿using DataAccessLayer.IDALs;
+using Microsoft.Data.SqlClient;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace DataAccessLayer.DALs
+{
+    public class DAL_PersonasADONET : IDAL_Personas
+    {
+        private string _connectionString = "Server=localhost,1433;Database=Practico2;User Id=sa;Password=Abc*123!;Encrypt=False;";
+
+        private Dictionary<string, Persona> personas = new Dictionary<string, Persona>();
+
+        public List<Persona> Get()
+        {
+            List<Persona> personas = new List<Persona>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Personas", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Persona persona = new Persona();
+
+                            persona.Documento = reader["Documento"].ToString();
+                            persona.Nombre = reader["Nombre"].ToString();
+
+                            personas.Add(persona);
+                        }
+                    }
+                }
+            }
+
+            return personas;
+        }
+
+        public Persona Get(string documento)
+        {
+            Persona persona = null;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Personas WHERE Documento = @Documento", connection))
+                {
+                    command.Parameters.AddWithValue("@Documento", documento);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            persona = new Persona();
+                            persona.Documento = reader["Documento"].ToString();
+                            persona.Nombre = reader["Nombre"].ToString();
+                        }
+                    }
+                }
+            }
+
+            return persona;
+        }
+
+        public void Insert(Persona persona)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("INSERT INTO Personas (Documento, Nombre) VALUES (@Documento, @Nombre)", connection))
+                {
+                    command.Parameters.AddWithValue("@Documento", persona.Documento);
+                    command.Parameters.AddWithValue("@Nombre", persona.Nombre);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Update(Persona persona)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("UPDATE Personas SET Nombre = @Nombre WHERE Documento = @Documento", connection))
+                {
+                    command.Parameters.AddWithValue("@Documento", persona.Documento);
+                    command.Parameters.AddWithValue("@Nombre", persona.Nombre);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(string documento)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("DELETE FROM Personas WHERE Documento = @Documento", connection))
+                {
+                    command.Parameters.AddWithValue("@Documento", documento);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ practico1/PracticoClase1/Commands.cs
@@ -0,0 +1,82 @@
+﻿using BusinessLayer.BLs;
+using BusinessLayer.IBLs;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticoClase1
+{
+    public class Commands
+    {
+        IBL_Personas _personasBL;
+
+        public Commands(IBL_Personas personasBL)
+        {
+            _personasBL = personasBL;
+        }
+
+        public void AddPersona()
+        {
+            // Pedimos los datos de la pesona.
+            Persona persona = new Persona();
+            Console.WriteLine("Ingrese el nombre de la persona: ");
+            persona.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el documento de la persona: ");
+            persona.Documento = Console.ReadLine();
+
+            _personasBL.Insert(persona);
+
+            _personasBL.Get(persona.Documento).Print();
+        }
+
+        public void ListPersonas()
+        {
+            List<Persona> personas = _personasBL.Get();
+
+            Console.WriteLine("Listado de personas:");
+            Console.WriteLine("| Documento | Nombre |");
+
+            foreach (Persona persona in personas)
+            {
+                persona.PrintTable();
+            }
+        }
+
+        public void RemovePersona()
+        {
+            Console.WriteLine("Ingrese el documento de la persona a eliminar: ");
+            string documento = Console.ReadLine();
+
+            _personasBL.Delete(documento);
+        }
+
+        public void UpdatePersona()
+        {
+            Console.WriteLine("Ingrese el documento de la persona que desea actualizar: ");
+            string documento = Console.ReadLine();
+
+            Persona personaExistente = _personasBL.Get(documento);
+
+            if (personaExistente != null)
+            {
+                Console.WriteLine("Ingrese el nuevo nombre de la persona: ");
+                string nuevoNombre = Console.ReadLine();
+
+                // Actualizo los datos de la persona
+                personaExistente.Nombre = nuevoNombre;
+
+                _personasBL.Update(personaExistente);
+
+                Console.WriteLine("Persona actualizada correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("No se encontró ninguna persona con ese documento.");
+            }
+        }
+
+    }
+}
